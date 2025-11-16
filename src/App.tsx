0427403c3ce23321c9599f0388ffa791fdbe2aa8
Zhumuyoutong <xs2415@columbia.edu>
2025-11-16 15:21:00 -0500

    change from next.js to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-'use client';
-
+import React, { useRef } from 'react';
+import QueryOutput from './components/QueryOutput/QueryOutput';
 import './globals.css';
-import { useRef, RefObject } from 'react';
-import QueryOutput from '../components/QueryOutput/QueryOutput';
 
 function App() {
     const APIEndPoint = "https://d31d96jtmy1k9d.cloudfront.net";
-    const SchemaExplorerRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null); // Reference to the left area
-    const DesignAreaRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null); // Reference to the right area
+    const SchemaExplorerRef = useRef<HTMLDivElement>(null);
+    const DesignAreaRef = useRef<HTMLDivElement>(null);
+    
     return (
         <div className="App">
             <div className="container">
                 <div className="schema-explorer" ref={SchemaExplorerRef}>
                     <h3>Which table do you want to query?</h3>
-                    {/* Schema content will go here */}
                 </div>
                 <div className="design-area" ref={DesignAreaRef}>
                     <p>Please drag a table from the left over here</p>
-                    {/* Design area content will go here */}
                 </div>
                 <QueryOutput>
                     
                 </QueryOutput>
-
             </div>
         </div>
     );
